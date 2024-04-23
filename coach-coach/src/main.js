5991ba8bfc8c237bc41b6fb675d69e88904f78d3
unknown <bseongwook>
2024-04-23 11:50:54 +0900

    [bug] refresh -> token 읽어오기

--- coach-coach/src/main.js
+++ coach-coach/src/main.js
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 이 파일이 진입점. 새로고침하면 여기서 초기화 작업 수행됨
+
+
+const token = sessionStorage.getItem('token');
+
+if (token) {
+    // 세션 스토리지에 토큰이 있으면 Vuex 스토어에 저장
+    store.commit('setToken', token);
+}
+
 createApp(App).use(store).use(router).mount('#app')
